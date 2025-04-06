8b172a9b13248bca41ca6e7e03757355974c5649
Deny <denylson.feitoza@alunos.ufersa.edu.br>
2025-04-06 18:00:03 -0300

    feat: add lightningcss for CSS minification, update Vercel configuration, and create .npmrc for peer dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    cssMinify: 'lightningcss',
     minify: 'terser',
     terserOptions: {
       compress: {
